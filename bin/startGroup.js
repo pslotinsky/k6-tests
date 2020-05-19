2f26647417f2e0f0e9fa6364b67470efd98d7546
Dima Panov <rpsdpano@gmail.com>
2020-05-19 16:51:12 +0300

    Fix start group script

--- bin/startGroup.js
+++ bin/startGroup.js
@@ -11,19 +11,25 @@ shell.exec(`rm -rf dist`);
 
 shell.exec(`yarn tsc`);
 
-shell.exec(`find dist -type f -exec sed -i '' 's/\\.ts"/\\.js"/g' {} +`);
+shell.exec(`find dist -type f -exec sed -i 's/\\.ts"/\\.js"/g' {} +`);
 
 Object.keys(compilerOptions.paths).forEach(key => {
     const name = key.substr(0, key.length - 2);
     const value = compilerOptions.paths[key][0].substr(0, key.length - 2);
     const absolutePath = path.resolve(__dirname, '../dist/' + value).replace(/\//g, '\\/');
 
-    shell.exec(`find dist -type f -exec sed -i '' 's/${name}/${absolutePath}/g' {} +`);
+    shell.exec(`find dist -type f -exec sed -i 's/${name}/${absolutePath}/g' {} +`);
 });
 
-(async function() {
+const moduleDirName = process.argv[2];
+
+shell.exec(`mkdir -p results`);
+shell.exec(`mkdir -p results/${moduleDirName}`);
+
+(async () => {
     for (let loaderName of loaderNames) {
-        shell.exec(`k6 run --compatibility-mode=base -e LOADER=${loaderName} ./dist/index.js`);
+        shell.exec(`k6 run --summary-export=results/${moduleDirName}/${loaderName}.json \
+         --compatibility-mode=base -e LOADER=${moduleDirName}/loader/${loaderName} ./dist/index.js`);
         await sleep(15000);
     }
 })();
